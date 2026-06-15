fd2817a2065857ae7e094fa71af51eae76415225
Mirzaahmedov Bekzod <mirzaahmedov.dev@gmail.com>
2026-06-15 09:01:42 +0500

    fix user schema

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -1,9 +1,9 @@
 import z from "zod";
 
 export const UserCreateSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().nonempty(),
   email: z.email(),
-  password: z.string().min(6),
+  password: z.string().nonempty(),
 });
 
 export const UserLoginSchema = z.object({
@@ -14,6 +14,6 @@ export const UserLoginSchema = z.object({
 export const UserUpdateSchema = z.object({
   name: z.string().min(1).optional(),
   email: z.email().optional(),
-  password: z.string().min(6).optional(),
+  password: z.string().optional(),
   status: z.enum(["unverified", "blocked", "active"]).optional(),
 });
